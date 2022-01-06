713f1f55ae65713d1621a9ddf43c3a4e2008a08c
cristofolinilucas <cristofolinilucas@gmail.com>
2022-01-06 01:35:03 -0300

    Capitulo 2 variaveis tomada de decisao e lacos de repeticao

---
+++ Capitulo_2/decisaoSimples.py
@@ -0,0 +1,8 @@
+nome = input("Digite o nome: ")
+idade = int(input("Digite a idade: "))
+prioridade = "NÃO"
+
+if idade>=65:
+    prioridade = "SIM"
+
+print("O paciente " + nome + " possui atendimento prioritario ? " + prioridade)
---
+++ Capitulo_2/variaveis2.py
@@ -0,0 +1,6 @@
+nome1 = input("Digite o nome do responsavel: ")
+nome2 = input("Digite o nome do destinatario: ")
+evento = input("Digite o nome do evento: ")
+valor_entrada = float(input("Digite o valor de entrada: "))
+
+print("Declaro para o senhor " + nome1 + " que o senhor " + nome2 + " esteve presente no evento " + evento + " e gastou o valor de R$ " + str(valor_entrada) + " com a entrada.")
